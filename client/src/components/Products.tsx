89fb36f58a74f75432958184b2028269190f1887
arun24hrs <2017arun.rana@gmail.com>
2023-08-03 15:23:05 +0530

    added sorting functionality

--- client/src/components/Products.tsx
+++ client/src/components/Products.tsx
@@ -1,14 +1,15 @@
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
+  Grid,
   HStack,
   Heading,
-  Image,
   Input,
+  Select,
   Stack,
-  Text,
   VStack,
   useToast,
 } from "@chakra-ui/react";
@@ -23,6 +24,7 @@ import {
 } from "../redux/products/product.actions";
 import axios from "axios";
 import ProductCard from "./ProductCard";
+import { ProdcutStoreType } from "../redux/products/product.reducer";
 
 const Products = () => {
   const [page, setPage] = React.useState(1);
@@ -30,11 +32,11 @@ const Products = () => {
   const [image, setImage] = React.useState("");
   const [price, setPrice] = React.useState(0);
   const [description, setDescription] = React.useState("");
+  const [sort, setSort] = React.useState("");
   const [quantity, setQuantity] = React.useState(0);
-  const { isLoading, isError, products } = useAppSelector(
+  const { isLoading, isError, products }:ProdcutStoreType = useAppSelector(
     (store) => store.productReducer
     );
-    const [data,setData] = React.useState([products])
   const dispatch = useAppDispatch();
   const toast = useToast();
 
@@ -72,7 +74,7 @@ const Products = () => {
       axios
         .post("http://localhost:8080/products/add", newProd)
         .then((res) => dispatch(postProds()))
-        .catch((err) => dispatch(errorProds()));
+        .catch((err) => dispatch(errorProds())).finally(()=> getProductsData())
     } else {
       toast({
         title: "Form Incomplete!",
@@ -84,12 +86,13 @@ const Products = () => {
       });
     }
 
-    getProductsData();
+    
   };
 
   const getProductsData = () => {
+    dispatch(requestProds());
     axios
-      .get(`http://localhost:8080/products?page=${page}`)
+      .get(`http://localhost:8080/products/${sort}?page=${page}`)
       .then((res) => dispatch(getProds(res.data)))
       .catch((err) => dispatch(errorProds()));
   };
@@ -98,106 +101,119 @@ const Products = () => {
     setPage((prev) => prev + page);
   };
 
-  React.useEffect(() => {
-    setData(products)
+  const handleSortChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+    setSort(e.target.value);
     getProductsData();
-  }, [page, data]);
+    
+  }
 
-  return (
-    <HStack>
-      <VStack
-        maxW={"500px"}
-        borderRight={"1px solid gray"}
-        minH={"100vh"}
-        p={5}
-      >
-        <Heading size={"md"} m={"30px"}>
-          Add Products
-        </Heading>
-        <Stack spacing={4}>
-          <FormControl id="name">
-            <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              value={name}
-              name="name"
-              onChange={(e) => handleChange(e)}
-            />
-          </FormControl>
-          <FormControl id="image">
-            <FormLabel>Image URL</FormLabel>
-            <Input
-              type="text"
-              value={image}
-              name="image"
-              onChange={(e) => handleChange(e)}
-            />
-          </FormControl>
-          <FormControl id="price">
-            <FormLabel>Price</FormLabel>
-            <Input
-              type="nubmer"
-              value={price}
-              name="price"
-              onChange={(e) => handleChange(e)}
-            />
-          </FormControl>
-          <FormControl id="quantity">
-            <FormLabel>Quantity</FormLabel>
-            <Input
-              type="number"
-              value={quantity}
-              name="quantity"
-              onChange={(e) => handleChange(e)}
-            />
-          </FormControl>
-          <FormControl id="description">
-            <FormLabel>Description</FormLabel>
-            <Input
-              type="text"
-              value={description}
-              name="description"
-              onChange={(e) => handleChange(e)}
-            />
-          </FormControl>
-          <Stack spacing={10}>
-            <Button
-              onClick={handleAddProduct}
-              bg={"blue.400"}
-              color={"white"}
-              _hover={{
-                bg: "blue.500",
-              }}
-            >
-              {isLoading ? "Adding..." : "Add Product"}
-            </Button>
+
+  React.useEffect(() => {
+    getProductsData();
+  }, [page, sort]);
+
+    return (
+      <HStack>
+        <VStack
+        justifyContent={"flex-start"}
+          maxW={"500px"}
+          borderRight={"1px solid gray"}
+          minH={"100vh"}
+          p={5}
+        >
+          <Heading size={"md"} m={"30px"}>
+            Add Products
+          </Heading>
+          <Stack spacing={4}>
+            <FormControl id="name">
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                value={name}
+                name="name"
+                onChange={(e) => handleChange(e)}
+              />
+            </FormControl>
+            <FormControl id="image">
+              <FormLabel>Image URL</FormLabel>
+              <Input
+                type="text"
+                value={image}
+                name="image"
+                onChange={(e) => handleChange(e)}
+              />
+            </FormControl>
+            <FormControl id="price">
+              <FormLabel>Price</FormLabel>
+              <Input
+                type="nubmer"
+                value={price}
+                name="price"
+                onChange={(e) => handleChange(e)}
+              />
+            </FormControl>
+            <FormControl id="quantity">
+              <FormLabel>Quantity</FormLabel>
+              <Input
+                type="number"
+                value={quantity}
+                name="quantity"
+                onChange={(e) => handleChange(e)}
+              />
+            </FormControl>
+            <FormControl id="description">
+              <FormLabel>Description</FormLabel>
+              <Input
+                type="text"
+                value={description}
+                name="description"
+                onChange={(e) => handleChange(e)}
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Button
+                onClick={handleAddProduct}
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                {isLoading ? "Adding..." : "Add Product"}
+              </Button>
+            </Stack>
           </Stack>
-        </Stack>
-      </VStack>
-      <VStack alignContent={"center"} alignItems={"center"}>
-        <Box>
-          <Heading size={"lg"}>All Products</Heading>
-        </Box>
-        <HStack>
-          {
-            data.map((el:ProductType) => (
-              <ProductCard key=
-              {Date.now()+Math.random()} {...el}/>
-            ))}
-        </HStack>
-        <HStack>
-          <Button
-            isDisabled={page === 1 ? true : false}
-            onClick={() => handlePage(-1)}
-          >
-            Prev.
-          </Button>
-          <Button isDisabled={true}>{page}</Button>
-          <Button onClick={() => handlePage(1)}>Next</Button>
-        </HStack>
-      </VStack>
-    </HStack>
-  );
-};
+        </VStack>
+        <VStack alignContent={"center"} alignItems={"center"}>
+          <Box>
+            <Heading size={"lg"}>All Products</Heading>
+          </Box>
+          <Box>
+            <Select placeholder="Sort by Price" value={sort} onChange={(e)=> handleSortChange(e)}>
+              <option value={"priceHTL"}>High to Low</option>
+              <option value={"priceLTH"} >Low to High</option>
+            </Select>
+          </Box>
+          <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+            {
+              products.map((el:ProductType) => (
+                <ProductCard key=
+                {Date.now()+Math.random()} {...el}/>
+              ))}
+          </Grid>
+          <HStack>
+            <Button
+              isDisabled={page === 1 ? true : false}
+              onClick={() => handlePage(-1)}
+            >
+              Prev.
+            </Button>
+            <Button isDisabled={true}>{page}</Button>
+            <Button onClick={() => handlePage(1)}>Next</Button>
+          </HStack>
+        </VStack>
+      </HStack>
+    );
+  }
 
 export default Products;
